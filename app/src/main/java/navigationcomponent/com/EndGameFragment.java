74042aa08f2f1ad818871fe6c2dd0318a020badb
Siyanda <siyandatholi09@gmail.com>
2021-01-10 14:28:50 +0200

    Navigation Component

---
+++ app/src/main/java/navigationcomponent/com/EndGameFragment.java
@@ -0,0 +1,44 @@
+package navigationcomponent.com;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class EndGameFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_end_game, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NavController navController = Navigation.findNavController(view);
+
+        Button button  =view.findViewById(R.id.restart);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // navController.navigate(R.id.action_endGameFragment_to_startFragment2);
+                NavOptions navOptions =new NavOptions.Builder().setPopUpTo(R.id.start,true).build();
+                navController.navigate(R.id.action_endGameFragment_to_startFragment,null,navOptions);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/navigationcomponent/com/StartFragment.java
@@ -0,0 +1,45 @@
+package navigationcomponent.com;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class StartFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_start, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final NavController navController = Navigation.findNavController(view);
+        Button button =view.findViewById(R.id.start);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_startFragment_to_gameFragment);
+            }
+        });
+
+
+
+    }
+}
